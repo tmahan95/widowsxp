7a0a5adbf7bc179d6fd35f64a7b5bcba0f8a61d1
vagrant <vagrant@homestead>
2018-10-24 15:10:02 +0000

    changed logs table, new column to programs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -37,6 +37,7 @@
 						<th>BIOS Date</th>
 						<th>Model</th>
 						<th>Serial</th>
+						<th>Programs</th>
 					<tr>
 				</thead>
 				<tbody>
@@ -52,6 +53,7 @@
 							<td> <a href="{{ route('refinedLogSearch',['q'=>$logs->bios_date]) }}"> {{ $logs->bios_date }}</a></td>
 							<td> <a href="{{ route('refinedLogSearch',['q'=>$logs->model]) }}"> {{ $logs->model }}</a></td>
 							<td> <a href="{{ route('refinedLogSearch',['q'=>$logs->serial]) }}"> {{ $logs->serial }}</a></td>
+							<td> <a href="{{ route('refinedProgSearch', ['q'=>$logs->compname]) }}">See Programs</a></td>
 						</tr>
 					@endforeach
 				</tbody>
